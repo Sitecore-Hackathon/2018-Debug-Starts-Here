75f475e6681e3bb299a8a8dfd17b007c24ed08ec
SitecoreMaster <dylany@gmail.com>
2018-03-03 14:16:14 -0500

    Added a Page Content Component

---
+++ src/Feature/code/Controllers/PageContentController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Helixbase.Feature.PageContent.Controllers
+{
+    public class PageContentController
+    {
+        public PageContentController()
+        {
+
+        }
+    }
+}
