a3889f295bd839c4c16a489d0ce59fcc79865f7a
SitecoreMaster <dylany@gmail.com>
2018-03-03 13:39:20 -0500

    Check in Fixes to match models (+ Post) from Bhavesh's side

--- google-bot/fulfillment/index.js
+++ google-bot/fulfillment/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
 const http = require('http');
-const App = require('actions-on-google').DialogflowApp;
-const functions = require('firebase-functions');
 const host = 'http://hackathon.scweb.arrowdesigns.net/';
 
-exports.sitecore = functions.https.onRequest((request, response) => {
-  const app = new App({request, response});
+const $ = require('jquery');
+
+exports.dialogflowFirebaseFulfillment = (request, response) => {
   console.log('Request headers: ' + JSON.stringify(request.headers));
   console.log('Request body: ' + JSON.stringify(request.body));
   
@@ -16,31 +14,31 @@ exports.sitecore = functions.https.onRequest((request, response) => {
   let email = request.body.result.parameters['Email'];
   let restaurantType = request.body.result.parameters['restaurant-type'];
 
-  callSitecoreApi(app).then((output) => {
+  callSitecoreApi(location, email, restaurantType).then((output) => {
     response.setHeader('Content-Type', 'application/json');
     response.send(JSON.stringify({ 'speech': output, 'displayText': output}));
   }).catch((error) => {
     response.setHeader('Content-Type', 'application/json');
     response.send(JSON.stringify({ 'speech': error, 'displayText': error })); 
   });
-});
+}
 
 // c. The function that passes data to Sitecore xConnect
-function callSitecoreApi(app) {
+function callSitecoreApi(location, email, restaurantType) {
   return new Promise((resolve, reject) => {
     let path = '/xConnect/GoogleInteraction';
     
-    console.log('API Request' + host + path);
-    
-    http.get({host: host, path: path}, (res) => {
-        let body = '';
-        res.on('data', (d) => { body += d; });
-        res.on('end', () => {
-            // We don't return any data back for now
-        });
-        res.on('error', (error) => {
-            reject(error); 
-        });
+    $.ajax({
+       type: "POST",
+       url: host + path,
+       data: { location: location, email: email, restaurant_type: restaurantType },
+       success: function(data) {
+           // We don't return any data back for now
+           
+       },
+       error: function(error) {
+           reject(error);
+       }
     });
   })
-};
+}
